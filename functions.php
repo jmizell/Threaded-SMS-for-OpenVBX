ac3b6b032fc665cafd9d4edff918ef1dfe586e63
Jeremy Mizell <mizellj@ottercove.net>
2015-07-09 20:29:49 +0000

    attempted to improve page load performance, I think the bottle neck may be the api call, and not iterating over the array. Added comments

--- functions.php
+++ functions.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *Send a text only message to a recepient
+ *
+ *@param object Twilio account object
+ *@param string $to Recepient
+ *@param string $from Sender message
+ *@param string $message Message contents
+ *
+ *@return void
+ */
 function sendSMS($account, $to, $from, $content) {
   $account->messages->create(array( 
     'To' => "$to", 
@@ -7,6 +17,14 @@ function sendSMS($account, $to, $from, $content) {
   ));
 }
 
+/**
+ *Returns a first name, and last name associated with a phone number. This requires the address book
+ *    plugin
+ *
+ * @param string $number Number of contact to look up.
+ *
+ * @return string
+ */
 function lookupNumber($number) {
   $entry = PluginData::sqlQuery("select * from addressbook_contacts where phone='$number' limit 1");
   if ( count($entry) == 1 ) {
@@ -17,6 +35,13 @@ function lookupNumber($number) {
   }
 }
 
+/**
+ * Returns an array of phone numbers associated with the account
+ *
+ * @param object $account Twilio api object
+ *
+ * @return array
+ */
 function getAccountPhoneNumbers($account) {
   $numbers = $account->incoming_phone_numbers->getIterator(0, 50, array());
   foreach ($numbers as $number) {
@@ -25,10 +50,65 @@ function getAccountPhoneNumbers($account) {
   return $phoneNumbers;
 }
 
+/**
+ *Inserts into the database messages present in the api, but missing in sql, and returns the
+ *  inserted entry.
+ *
+ *@param array Message object from twilio api
+ *@param string Message sid
+ *
+ *@return array
+ */
+function insertMissingMessage($message, $sid) {
+  PluginData::sqlQuery("INSERT INTO messages SET created='" . $message->date_created . "', updated='', call_sid='". $message->sid . "', caller='" . $message->from . "', called='" . $message->to . "', type='sms', status='new', content_url='', content_text='" . $message->body . "', notes='', size='', assigned_to='', archived='0', ticket_status='',tenant_id='1'");
+  return PluginData::sqlQuery("SELECT * FROM messages WHERE call_sid='$sid'");
+}
 
-function getMessageThreads($account, $limit, $phoneNumbers) {
+/**
+ * Returns an array containing the selected details of a single message
+ *
+ * @param array Message object from twilio api
+ * @param string sid of message
+ * @param string the contact associated with the message
+ *
+ * @return array
+ */
+function getMessageArray($message, $sid, $contact) {
+  $result = PluginData::sqlQuery("SELECT * FROM messages WHERE call_sid='$sid'");
+  
+  if ( count($result) == 0 ) {
+    $result = insertMissingMessage($message, $sid);
+  }
+  
+  return Array(
+          'id' => $result[0]["id"],
+          'sid' => $sid,
+          'contact' => $contact,
+          'num_media' => $message->num_media,
+          'media' => $message->media,
+          'caller' => $message->from,
+          'created' => $message->date_created,
+          'status' => $result[0]["status"],
+          'archived' => $result[0]["archived"],
+          'body' => $message->body
+        );
+}
+
+/**
+ * Returns an multidimensional array containing contacts, and messages
+ *
+ * @param object $account The twilio account object
+ * @param int $limit How many messages to retreive from the account (broken)
+ * @param array $phoneNumbers Contains phone numbers associated with the account
+ * @param string $to Determines how much data is returned, set to empty only the first message of
+ *    each contact is returned, set to all, all messages are returned, and when a contact
+ *    phone number is specified, only the contacts messages are returned. Phone number is in the
+ *    form of +00000000000
+ *
+ * @return array
+ */
+function getThreads($account, $limit, $phoneNumbers, $to = "") {
   $messages = $account->messages->getIterator(0, $limit, array());
-  $timezone = PluginData::get("timezone",'UM8');
   
   $threads = array();
   foreach ($messages as $message) {
@@ -38,28 +118,36 @@ function getMessageThreads($account, $limit, $phoneNumbers) {
       $contact = $message->to;
     }
     $sid = $message->sid;
-    $result = PluginData::sqlQuery("SELECT * FROM messages WHERE call_sid='$sid'");
     
-    if ( count($result) == 0 ) {
-      PluginData::sqlQuery("INSERT INTO messages SET created='" . $message->date_created . "', updated='', call_sid='". $message->sid . "', caller='" . $message->from . "', called='" . $message->to . "', type='sms', status='new', content_url='', content_text='" . $message->body . "', notes='', size='', assigned_to='', archived='0', ticket_status='',tenant_id='1'");
-      $result = PluginData::sqlQuery("SELECT * FROM messages WHERE call_sid='$sid'");
+    switch ($to) {
+      case "":
+        if (empty($threads[$contact])) {
+          $message_array = getMessageArray($message, $sid, $contact);
+        
+          if ( $message_array["archived"] == 0 ) {
+            $threads[$contact][] = $message_array;
+          }
+        }
+        break;
+      case "all":
+        $message_array = getMessageArray($message, $sid, $contact);
+      
+        if ( $message_array["archived"] == 0 ) {
+          $threads[$contact][] = $message_array;
+        }
+        break;
+      default:
+        if ($contact == $to) {
+          $message_array = getMessageArray($message, $sid, $contact);
+        
+          if ( $message_array["archived"] == 0 ) {
+            $threads[$contact][] = $message_array;
+          }
+        }
     }
-  
-    if ( $result[0]["archived"] == 0 ) {
-      $threads[$contact][] = Array(
-        'id' => $result[0]["id"],
-        'sid' => $sid,
-        'contact' => $contact,
-        'num_media' => $message->num_media,
-        'media' => $message->media,
-        'caller' => $message->from,
-        'created' => $message->date_created,
-        'status' => $result[0]["status"],
-        'archived' => $result[0]["archived"],
-        'body' => $message->body
-      );
-    }
+    
   }
   return $threads;
 }
+
 ?>
--- plugin.php
+++ plugin.php
@@ -4,6 +4,7 @@ include("functions.php");
 $account = OpenVBX::getAccount();
 $phoneNumbers = getAccountPhoneNumbers($account);
 
+//detect if the address book is present, if so then we can attempt to lookup contacts
 if (count(PluginData::sqlQuery("select * from addressbook_contacts limit 1")) == 1) {
   $address_book = True;
 } else {
@@ -14,6 +15,7 @@ $to = filter_var($_REQUEST['to'], FILTER_SANITIZE_NUMBER_FLOAT);
 $from    = filter_var($_REQUEST['from'], FILTER_SANITIZE_NUMBER_FLOAT);
 $content = filter_var($_REQUEST['content'], FILTER_SANITIZE_STRING);
 
+// if the to field is set, we're viewing a single thried
 if ( $to != "" ) {
   $function = "contact";
   if ( $content != "" && $from != "") {
@@ -21,20 +23,19 @@ if ( $to != "" ) {
   }
 }
 
-// Set a limit of items per page
-$limit = 100;
-$threads_per_page = 20;
-$threads_offset = filter_var($_REQUEST['offset'], FILTER_SANITIZE_NUMBER_INT);
+$limit = 100; // Set a limit of items per page
+$threads_per_page = 20; // limit of items to display for page, contacts or messages
+$threads_offset = filter_var($_REQUEST['offset'], FILTER_SANITIZE_NUMBER_INT); //offset from begining of messages to display
 if ( $threads_offset == "" ) {
-  $threads_offset = 0;
+  $threads_offset = 0; //default offset value 
 }
-$allthreads = getMessageThreads($account, $limit, $phoneNumbers);
-
 
 if ( $function == "contact" ) {
+  $allthreads = getThreads($account, $limit, $phoneNumbers, $to);
   $threads = array_slice($allthreads[$to], $threads_offset, $threads_per_page);
   include("view-single-thread.php"); 
 } else {
+  $allthreads = getThreads($account, $limit, $phoneNumbers);
   $threads = array_slice($allthreads, $threads_offset, $threads_per_page);
   include("view-threads-list.php"); 
 }
